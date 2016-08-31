bf96e5cc1595d2e64d78c00093903886ac575d5c
llj <lingjun.li1@gmail.com>
2016-08-31 16:10:46 +0800

    [jstree] updated jstree to the latest v3.3.2, rewrote/modified related code, & bugfix

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -45,10 +45,11 @@ require.config({
         'js.cookie': 'lib/js.cookie',
 
         simplemodal: 'lib/jquery.simplemodal',
-        jstree: 'lib/jstree.1.0',
+        jstree: 'lib/jstree.min',
         select2: 'lib/select2-3.5.2',
         moment: 'lib/moment-with-locales',
         marked: 'lib/marked.min',
+        sha1: 'lib/crypto-js.sha1',
 
         underscore: 'lib/underscore',
         backbone: 'lib/backbone',
